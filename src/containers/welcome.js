95b6c5c849d7d7530ad396a5a4d0620c6df71392
jorge david hernandez diaz <jdaviderb@gmail.com>
2017-08-19 19:40:17 -0400

    move welcome styles to a file

--- src/containers/welcome.js
+++ src/containers/welcome.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
-import {
-  StyleSheet,
-  View,
-  Text,
-  Image
-} from 'react-native'
+import { View } from 'react-native'
+import styles from '../styles/welcome'
 import Button from 'apsl-react-native-button'
 import Slider from '../components/slider'
 import { connect } from 'react-redux'
@@ -32,30 +28,6 @@ const welcome =  ({slides}) => (
   </View>
 
 )
-
-const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1
-  },
-  button: {
-    flex: 1,
-    height: '100%', 
-    borderWidth: 0, 
-    backgroundColor: '#2C9BF4', 
-    borderRadius: 0, 
-    margin: 0, 
-    padding: 0
-  },
-  buttonText: {
-    fontSize: 14,
-    color: 'white'
-  },
-  options: {
-    flexDirection: 'row',
-    minHeight: '8%'
-  }
-})
-
 
 const mapStateToProps  = ({welcome}) => ({
   slides: welcome
---
+++ src/styles/welcome.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native'
+export default StyleSheet.create({
+  wrapper: {
+    flex: 1
+  },
+  button: {
+    flex: 1,
+    height: '100%', 
+    borderWidth: 0, 
+    backgroundColor: '#2C9BF4', 
+    borderRadius: 0, 
+    margin: 0, 
+    padding: 0
+  },
+  buttonText: {
+    fontSize: 14,
+    color: 'white'
+  },
+  options: {
+    flexDirection: 'row',
+    minHeight: '8%'
+  }
+})
